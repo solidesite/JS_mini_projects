b6d8557d1432f69c6e746e674c7d18bc41075f83
solidesite <solidesite@gmail.com>
2021-07-31 01:52:40 +0900

    feat : Add down, reset function

--- Counter/js/counter.js
+++ Counter/js/counter.js
@@ -2,24 +2,21 @@ const up = document.querySelector(".btn_up")
 const down = document.querySelector(".btn_down")
 const reset = document.querySelector(".btn_reset")
 const counter = document.querySelector(".counter")
-const box = document.querySelector(".box")
-
-// const counting = 1;
-// const counting2 = String(counting);
-// const counting = function () {
-//   const num = 0;
-//   num++;
-// };
-// const number = 1;
-
 let num = 0;
-// up.innerText === counting.innerText;
+
 up.addEventListener("click", () => {
   num++;
   counter.innerText = num;
 })
 down.addEventListener("click", () => {
-  
+  if (num > 0) {
+    num--;
+    counter.innerText = num;
+  }
+})
+reset.addEventListener("click", () => {
+  num = 0;
+  counter.innerText = num;
 })
 box.addEventListener("click", () => {
   box.classList.add("red")
